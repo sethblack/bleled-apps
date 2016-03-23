2b21df0d1c1a52791ab593fc81d8af73e2ec469f
Seth Black <sblack@sethserver.com>
2016-03-22 22:40:35 -0500

    just connect to send messages then disconnect

--- android/app/src/main/java/com/sethserver/blecandle/BLECandle.java
+++ android/app/src/main/java/com/sethserver/blecandle/BLECandle.java
@@ -25,6 +25,10 @@ public class BLECandle {
     private Context mContext;
     private BluetoothDevice mDevice;
     private boolean mConnected = false;
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_READY = 1;
+    private static final int STATE_WAITING = 2;
+    private int mState = STATE_DISCONNECTED;
 
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
         @Override
@@ -35,16 +39,19 @@ public class BLECandle {
                         mBluetoothGatt.discoverServices());
 
                 mConnected = true;
+                mState = STATE_DISCONNECTED;
 
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 Log.i(TAG, "Disconnected from GATT server.");
                 mConnected = false;
+                mState = STATE_READY;
             }
         }
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             if (status == BluetoothGatt.GATT_SUCCESS) {
+                mState = STATE_WAITING;
                 for (BluetoothGattService s : gatt.getServices()) {
                     Log.i(TAG, "Serivce: " + s.getUuid().toString());
 
@@ -57,9 +64,16 @@ public class BLECandle {
                         }
                     }
                 }
+                mState = STATE_READY;
             } else {
                 Log.w(TAG, "onServicesDiscovered received: " + status);
             }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            Log.i(TAG, "onCharacteristicWrite");
+            BLECandle.this.mState = BLECandle.STATE_READY;
         }
     };
 
@@ -68,8 +82,26 @@ public class BLECandle {
         mDevice = device;
     }
 
+    public boolean isBusy() {
+        return !(mState == STATE_READY);
+    }
+
+    public String getDeviceAddress() {
+        return mDevice.getAddress();
+    }
+
     public void connectGatt() {
         mBluetoothGatt = mDevice.connectGatt(mContext, false, mGattCallback);
+    }
+
+    public void closeGatt() {
+        mBluetoothGatt.close();
+        mBluetoothGatt = null;
+        mCharacteristic = null;
+    }
+
+    public boolean hasCharacteristic() {
+        return mCharacteristic != null;
     }
 
     public boolean isConnected() {
@@ -81,6 +113,9 @@ public class BLECandle {
 
         if (mCharacteristic == null) {
             Log.i(TAG, "Can't write to null Characteristic");
+
+            this.connectGatt();
+
             return false;
         }
 
@@ -90,6 +125,8 @@ public class BLECandle {
             Log.w(TAG, "Failed to write characteristic");
             return false;
         }
+
+        mState = STATE_WAITING;
 
         return true;
     }
--- android/app/src/main/java/com/sethserver/blecandle/MainActivity.java
+++ android/app/src/main/java/com/sethserver/blecandle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sethserver.blecandle;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -7,7 +8,11 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.SeekBar;
@@ -18,6 +23,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
+    private final static int PERMISSION_COARSE_LOCATION = 10;
     private final static String TAG = "MainActivity";
     private BluetoothAdapter mBluetoothAdapter;
     private ArrayList<BLECandle> mDiscoveredCandles;
@@ -47,9 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.i(TAG, "Progress: " + mProgress);
 
-                for (BLECandle candle : mDiscoveredCandles) {
-                    candle.writeToCharacteristic(mProgress);
-                }
+                sendMessageToAllConnectedDevices(mProgress);
             }
 
             @Override
@@ -70,11 +74,79 @@ public class MainActivity extends AppCompatActivity {
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return;
         }
 
         mDiscoveredCandles = new ArrayList<BLECandle>();
 
-        mBluetoothAdapter.getBluetoothLeScanner().startScan(leScanCallback);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_COARSE_LOCATION);
+        } else {
+            mBluetoothAdapter.getBluetoothLeScanner().startScan(leScanCallback);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_COARSE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mBluetoothAdapter.getBluetoothLeScanner().startScan(leScanCallback);
+                }
+            }
+        }
+    }
+
+    public void sendMessageToAllConnectedDevices(int value) {
+        final int inValue = value;
+
+        class myRunnable implements Runnable {
+            public void run() {
+                for (BLECandle candle : mDiscoveredCandles) {
+                    candle.connectGatt();
+
+                    int sleepTicks = 0;
+
+                    while(!candle.hasCharacteristic()) {
+                        SystemClock.sleep(25);
+
+                        sleepTicks += 1;
+
+                        if (sleepTicks > 80) {
+                            Log.w(TAG, "Candle " + candle.getDeviceAddress() + " took too long to connect");
+                            break;
+                        }
+                    }
+
+                    candle.writeToCharacteristic(inValue);
+
+                    sleepTicks = 0;
+
+                    while(candle.isBusy()) {
+                        SystemClock.sleep(25);
+
+                        sleepTicks += 1;
+
+                        if (sleepTicks > 80) {
+                            Log.w(TAG, "Candle " + candle.getDeviceAddress() + " took too long to respond to the write");
+                            break;
+                        }
+                    }
+
+                    candle.closeGatt();
+
+                    SystemClock.sleep(10);
+                }
+            }
+        }
+
+        new Thread(new myRunnable()).start();
     }
 
     private ScanCallback leScanCallback = new ScanCallback() {
@@ -91,12 +163,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (deviceName.equalsIgnoreCase("bleled")) {
-                Log.i(TAG, deviceName);
-                BLECandle candle = new BLECandle(device, MainActivity.this);
+                Log.i(TAG, "name: " + deviceName);
+                Log.i(TAG, "address: " + device.getAddress());
+                Log.i(TAG, "hashCode: " + device.hashCode());
 
-                candle.connectGatt();
+                for (BLECandle c : MainActivity.this.mDiscoveredCandles) {
+                    if (c.getDeviceAddress().equals(device.getAddress())) {
+                        return;
+                    }
+                }
+
+                BLECandle candle = new BLECandle(device, MainActivity.this);
+                //candle.connectGatt();
 
                 MainActivity.this.mDiscoveredCandles.add(candle);
+
                 MainActivity.this.mCandlesFound.setText(
                         "Candles Found: " + MainActivity.this.mDiscoveredCandles.size()
                 );
